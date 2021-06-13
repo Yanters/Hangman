754485445de2c479026d13ac47ef078a6612840e
43695512 <yanter@wp.pl>
2021-06-13 17:40:51 +0200

    Initial commit

---
+++ Hangman/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace Hangman
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.Restart = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.Location = new System.Drawing.Point(64, 216);
+            this.textBox1.MaxLength = 1;
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(305, 33);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "Wpisz literę";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Poor Richard", 20.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(64, 286);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(305, 83);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Sprawdz litere";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Impact", 26.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(61, 107);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 43);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "_";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Impact", 26.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(105, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 43);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "_";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Impact", 26.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(149, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 43);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "_";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Impact", 26.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(193, 107);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 43);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "_";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Impact", 26.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(237, 107);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 43);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "_";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Impact", 26.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.Location = new System.Drawing.Point(281, 107);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 43);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "_";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Impact", 26.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label7.Location = new System.Drawing.Point(331, 107);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(38, 43);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "_";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Hangman.Properties.Resources._0;
+            this.pictureBox2.Location = new System.Drawing.Point(421, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(367, 407);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // Restart
+            // 
+            this.Restart.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Restart.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Restart.Location = new System.Drawing.Point(140, 396);
+            this.Restart.Name = "Restart";
+            this.Restart.Size = new System.Drawing.Size(135, 23);
+            this.Restart.TabIndex = 11;
+            this.Restart.Text = "RESTART";
+            this.Restart.UseVisualStyleBackColor = true;
+            this.Restart.Visible = false;
+            this.Restart.Click += new System.EventHandler(this.Restart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Restart);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Hangman";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button Restart;
+    }
+}
+
---
+++ Hangman/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hangman
+{
+    public partial class Form1 : Form
+    {
+        String slowo;
+        int ile_pudel = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            losuj_slowo();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string litera = textBox1.Text;
+            bool czy_trafiony = false;
+            int gdzie_trafiony = 0;
+            if (ile_pudel < 5)
+            {
+                for (int i = 1; i < 6; i++)
+                {
+                    if (Convert.ToString(slowo[i]) == litera)
+                    {
+                        czy_trafiony = true;
+                        gdzie_trafiony = i;
+                    }
+                    if (gdzie_trafiony == 1) { label2.Text = litera; }
+                    if (gdzie_trafiony == 2) { label3.Text = litera; }
+                    if (gdzie_trafiony == 3) { label4.Text = litera; }
+                    if (gdzie_trafiony == 4) { label5.Text = litera; }
+                    if (gdzie_trafiony == 5) { label6.Text = litera; }
+                }
+
+                if (czy_trafiony == false)
+                {
+                    ile_pudel++;
+                    if (ile_pudel == 1) { pictureBox2.Image = Hangman.Properties.Resources.B; }
+                    if (ile_pudel == 2) { pictureBox2.Image = Hangman.Properties.Resources.C; }
+                    if (ile_pudel == 3) { pictureBox2.Image = Hangman.Properties.Resources.D; }
+                    if (ile_pudel == 4) { pictureBox2.Image = Hangman.Properties.Resources.E; }
+                    if (ile_pudel == 5) { pictureBox2.Image = Hangman.Properties.Resources.F; }
+                    if (ile_pudel == 6) { pictureBox2.Image = Hangman.Properties.Resources.G; }
+                    if (ile_pudel == 7) { pictureBox2.Image = Hangman.Properties.Resources.H; }
+                    if (ile_pudel == 8) { pictureBox2.Image = Hangman.Properties.Resources.I; }
+                    if (ile_pudel == 9) { pictureBox2.Image = Hangman.Properties.Resources.J; }
+                    if (ile_pudel == 10) { pictureBox2.Image = Hangman.Properties.Resources.K; Restart.Visible = true; }
+
+                }
+                wygrana();
+                
+            }
+            else
+            {
+                pictureBox2.Image = Hangman.Properties.Resources.LOSSER;
+                Restart.Visible = true;
+            }
+            
+        }
+
+        private void losuj_slowo()
+        {
+            
+            string[] slowa = { "krokusy", "liliput", "marchew", "selerek", "krakers", "klakier", "akwarel","Karinka" };
+            int ile_slow = slowa.Length;
+            Random gen = new Random();
+            int indeks_slowa = gen.Next(0, ile_slow - 1);
+            slowo = slowa[indeks_slowa];
+            label1.Text = Convert.ToString(slowo[0]);
+            label7.Text = Convert.ToString(slowo[6]);
+
+
+                }
+        private void wygrana()
+        {
+            if(label2.Text!="_")
+            {
+                if (label3.Text != "_")
+                {
+                    if (label4.Text != "_")
+                    {
+                        if (label5.Text != "_")
+                        {
+                            if (label6.Text != "_")
+                            {
+                                pictureBox2.Image = Hangman.Properties.Resources.WINNER;
+                                Restart.Visible = true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Restart_Click(object sender, EventArgs e)
+        {
+            losuj_slowo();
+            label2.Text = "_";
+            label3.Text = "_";
+            label4.Text = "_";
+            label5.Text = "_";
+            label6.Text = "_";
+            ile_pudel = 0;
+            Restart.Visible = false;
+
+
+            pictureBox2.Image = Hangman.Properties.Resources._0;
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
